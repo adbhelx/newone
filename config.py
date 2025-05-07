38bde8c7daf9ef959fb9bc2979175d04874d6735
0onepro <0onemajed@gmail.com>
2025-05-07 03:21:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "8085016643:AAH0-3wNlvv89wOWxaO6uMMQD6h7jzMkowE"  # ضع هنا التوكن الخاص بك
+TOKEN = "8085016643:AAEHAO1BlQzhdo39N7MSkx3NEZK3P0d5M58"  # ضع هنا التوكن الخاص بك
 ADMIN_USER_IDS = [953696547, 7942066919]  # ضع هنا معرفات المستخدمين المشرفين
